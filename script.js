87f4620daf9b9cff3c0fc82c2fdd6a71fa5aac18
jhampier12 <ruizreyesjam@gmail.com>
2025-02-16 20:34:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function mostrarLineaTiempo() {
   window.location.href = "timeline.html";
 }
 function iniciarJuego() {
-  window.location.href = "juegosDeRecuerdos.html";
+  window.location.href = "juegosderecuerdos.html";
 }
 function mostrarCarta() {
-  window.location.href = "cartaEspecial.html";
+  window.location.href = "cartaespecial.html";
 }
 function mostrarRazones() {
   window.location.href = "razones.html";
